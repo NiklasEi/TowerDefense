1728e5385a838dc582f151bf34af2e8cf905fa9e
Niklas Eicker <nikle1@gmx.de>
2017-09-18 23:23:36 +0200

    fix number format exception + deSerializeLoc method

--- src/main/java/me/nikl/towerdefense/util/StringUtil.java
+++ src/main/java/me/nikl/towerdefense/util/StringUtil.java
@@ -1,7 +1,11 @@
 package me.nikl.towerdefense.util;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
+
+import java.util.Locale;
 
 /**
  * Created by Niklas on 18.09.2017.
@@ -13,12 +17,36 @@ public class StringUtil {
     }
 
     public static String serializeLoc(Location loc){
-        return loc.getWorld().getName() + ":" + String.format("%.2f", loc.getX()) +
-                ":" + String.format("%.2f", loc.getY()) + ":" + String.format("%.2f", loc.getZ()) +
-                ":" + String.format("%.2f", loc.getPitch()) + ":" + String.format("%.2f", loc.getYaw());
+        if(loc == null) return "null";
+        return loc.getWorld().getName() + ":" + String.format(Locale.US,"%.2f", loc.getX()) +
+                ":" + String.format(Locale.US,"%.2f", loc.getY()) + ":" + String.format(Locale.US,"%.2f", loc.getZ()) +
+                ":" + String.format(Locale.US,"%.2f", loc.getPitch()) + ":" + String.format(Locale.US,"%.2f", loc.getYaw());
     }
 
     public static Location deSerializeLoc(String locationString){
+        String[] locSplit = locationString.split(":");
+
+        if(locSplit.length != 6) return null;
+
+        World world = Bukkit.getWorld(locSplit[0]);
+
+        double x,y,z;
+        float pitch, yaw;
+
+        try {
+            x = Double.valueOf(locSplit[1]);
+            y = Double.valueOf(locSplit[2]);
+            z = Double.valueOf(locSplit[3]);
+
+            pitch = Float.valueOf(locSplit[4]);
+            yaw = Float.valueOf(locSplit[5]);
+
+            return new Location(world, x, y, z, pitch, yaw);
+
+        } catch (NumberFormatException exception){
+            exception.printStackTrace();
+        }
+
         return null;
     }
 
