976bba4b8fefb8f9e9300f693f4c901284bf3baa
Niklas Eicker <nikle1@gmx.de>
2017-09-18 23:22:54 +0200

    class for Zombies with wraper for NPC

--- src/main/java/me/nikl/towerdefense/npc/monster/Monster.java
+++ src/main/java/me/nikl/towerdefense/npc/monster/Monster.java
@@ -1,8 +1,21 @@
 package me.nikl.towerdefense.npc.monster;
 
+import me.nikl.towerdefense.arena.Arena;
+import me.nikl.towerdefense.npc.TDnpc;
+import net.citizensnpcs.Citizens;
+
 /**
  * Created by Niklas on 18.09.2017.
  */
 public enum Monster {
-    ZOMBIE
+    ZOMBIE;
+
+
+    public TDnpc getNPC(Arena arena) {
+        switch (this.toString()){
+            case "ZOMBIE":
+                return new Zombie(arena);
+        }
+        return null;
+    }
 }
--- src/main/java/me/nikl/towerdefense/npc/monster/Zombie.java
+++ src/main/java/me/nikl/towerdefense/npc/monster/Zombie.java
@@ -1,15 +1,23 @@
 package me.nikl.towerdefense.npc.monster;
 
+import me.nikl.towerdefense.arena.Arena;
 import me.nikl.towerdefense.npc.TDnpc;
 import net.citizensnpcs.Citizens;
 import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
 
 /**
  * Created by Niklas on 18.09.2017.
+ *
+ *
  */
 public class Zombie extends TDnpc {
+    private Arena arena;
 
-    public Zombie(Citizens citizens){
-        super(citizens.getNPCRegistry().createNPC(EntityType.ZOMBIE, "blub"));
+    public Zombie(Arena arena){
+        super(arena.getCitizens().getNPCRegistry().createNPC(EntityType.ZOMBIE, "blub"));
+
+        this.arena = arena;
+        this.path = arena.getPath();
     }
 }
