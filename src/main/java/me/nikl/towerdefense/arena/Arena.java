7a77aaa6e1dbbbeb5886c8831ae501a1dd47752c
Niklas Eicker <nikle1@gmx.de>
2017-09-18 23:19:20 +0200

    save arena on disable and load all arenas onEnable

--- src/main/java/me/nikl/towerdefense/arena/Arena.java
+++ src/main/java/me/nikl/towerdefense/arena/Arena.java
@@ -1,16 +1,22 @@
 package me.nikl.towerdefense.arena;
 
 import me.nikl.towerdefense.Main;
-import me.nikl.towerdefense.npc.TDnpc;
 import me.nikl.towerdefense.npc.monster.Monster;
+import me.nikl.towerdefense.util.StringUtil;
 import me.nikl.towerdefense.wave.Wave;
-import me.nikl.towerdefense.npc.monster.Zombie;
 import net.citizensnpcs.Citizens;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 
 /**
@@ -24,31 +30,32 @@ public class Arena {
 
     private String name;
 
-    private HashMap<Integer, Location> path = new HashMap<>();
+    private LinkedList<Location> path = new LinkedList<>();
 
     private LinkedList<Wave> waves = new LinkedList<>();
     private int currentWave = 0;
 
     private Citizens citizens;
-    private TDnpc monster;
 
     public Arena(Main plugin, String arenaName){
         this.plugin = plugin;
+        citizens = (Citizens) plugin.getServer().getPluginManager().getPlugin("Citizens");
         this.name = arenaName;
 
-        LinkedHashMap<Monster, Integer> monsters = new LinkedHashMap<>();
-        monsters.put(Monster.ZOMBIE, 10);
+        LinkedList<Monster> monsters = new LinkedList<>();
+        monsters.add(Monster.ZOMBIE);
         waves.add(new Wave(this, monsters));
 
+        LinkedList<Monster> monsters2 = new LinkedList<>();
+        monsters2.add(Monster.ZOMBIE);
+        monsters2.add(Monster.ZOMBIE);
+        waves.add(new Wave(this, monsters2));
+
         this.status = ArenaStatus.UNINITIALIZED;
-
-        citizens = (Citizens) plugin.getServer().getPluginManager().getPlugin("Citizens");
-
-        monster = new Zombie(citizens);
     }
 
     public void addLocationToPath(Location loc){
-        path.put(path.size(), loc);
+        path.add(loc);
     }
 
     /**
@@ -61,7 +68,7 @@ public class Arena {
 
         World world = path.get(0).getWorld();
 
-        for(Location loc : path.values()){
+        for(Location loc : path){
             if(!loc.getWorld().equals(world)) return "The locations are not in the same world!;Clear the arena by /tda arenaName clear;Then start from the beginning.";
         }
 
@@ -74,22 +81,83 @@ public class Arena {
     }
 
     public void clear() {
+        if(status == ArenaStatus.WAVE) waves.get(currentWave).shutDown();
         status = ArenaStatus.UNINITIALIZED;
         path.clear();
-        if(monster.getNPC().isSpawned()) monster.getNPC().despawn();
     }
 
     public boolean start(){
         if(status != ArenaStatus.STOPED) return false;
 
-        Main.debug("Spawning...");
-        monster.getNPC().spawn(path.get(0));
-        monster.getNPC().getBukkitEntity().setCustomNameVisible(false);
-        monster.getNPC().getNavigator().setTarget(path.get(1));
+        waves.get(currentWave).start();
+        plugin.getArenaManager().getWavesManager().addWave(this, waves.get(currentWave));
+        status = ArenaStatus.WAVE;
         return true;
     }
 
     public void shutDown() {
-        if(monster.getNPC().isSpawned()) monster.getNPC().despawn();
+        if(status == ArenaStatus.UNINITIALIZED) return;
+
+        waves.get(currentWave).shutDown();
+
+        File saveFile = new File(plugin.getDataFolder().toString() + File.separatorChar + "arenas" + File.separatorChar + name + ".yml");
+        if(!saveFile.exists()){
+            saveFile.getParentFile().mkdirs();
+            try {
+                saveFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+        FileConfiguration save;
+        try {
+            save = YamlConfiguration.loadConfiguration(new InputStreamReader(new FileInputStream(saveFile), "UTF-8"));
+        } catch (UnsupportedEncodingException | FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        save.set("loc", null);
+
+        int index = 0;
+
+        for(Location loc : path) {
+            save.set("loc." + index, StringUtil.serializeLoc(loc));
+            index++;
+        }
+
+        try {
+            save.save(saveFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Location getSpawn() {
+        return path.get(0);
+    }
+
+    public Citizens getCitizens() {
+        return citizens;
+    }
+
+    public Wave getWave() {
+        return waves.get(currentWave);
+    }
+
+    public LinkedList<Location> getPath() {
+        return path;
+    }
+
+    public void nextWave() {
+        plugin.getArenaManager().getWavesManager().removeWave(this);
+        if(currentWave >= waves.size() - 1) {
+            Bukkit.getConsoleSender().sendMessage("  Waves all through! Game is done!");
+            return;
+        }
+        currentWave++;
+        waves.get(currentWave).start();
+        plugin.getArenaManager().getWavesManager().addWave(this, waves.get(currentWave));
     }
 }
--- src/main/java/me/nikl/towerdefense/arena/ArenaManager.java
+++ src/main/java/me/nikl/towerdefense/arena/ArenaManager.java
@@ -1,7 +1,16 @@
 package me.nikl.towerdefense.arena;
 
 import me.nikl.towerdefense.Main;
+import me.nikl.towerdefense.util.StringUtil;
+import me.nikl.towerdefense.wave.WavesManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
 /**
@@ -11,10 +20,53 @@ public class ArenaManager {
 
     private Main plugin;
 
+    private WavesManager wavesManager;
+
     private HashMap<String, Arena> arenas = new HashMap<>();
 
     public ArenaManager(Main plugin){
         this.plugin = plugin;
+        this.wavesManager = new WavesManager(plugin);
+
+        loadArenas();
+    }
+
+    private void loadArenas() {
+        File arenaFolder = new File(plugin.getDataFolder().toString() + File.separatorChar + "arenas");
+        if(!arenaFolder.exists()){
+            arenaFolder.mkdirs();
+        }
+
+        String[] split;
+        for(File file : arenaFolder.listFiles()){
+            split = file.getName().split("\\.");
+
+            if(!split[split.length - 1].equals("yml")) continue;
+
+            loadArena(file, split[0]);
+        }
+    }
+
+    private void loadArena(File file, String arenaName) {
+        Arena arena = new Arena(plugin, arenaName);
+
+        FileConfiguration save;
+        try {
+            save = YamlConfiguration.loadConfiguration(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+        } catch (UnsupportedEncodingException | FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        for(String index : save.getConfigurationSection("loc").getKeys(false)){
+            arena.addLocationToPath(StringUtil.deSerializeLoc(save.getString("loc." + index)));
+
+            Main.debug(StringUtil.serializeLoc(StringUtil.deSerializeLoc(save.getString("loc." + index))));
+        }
+
+        arena.initialize();
+
+        arenas.put(arenaName, arena);
     }
 
     public boolean createArena(String arenaName){
@@ -33,5 +85,9 @@ public class ArenaManager {
             arena.shutDown();
         }
         // ToDo: save arenas
+    }
+
+    public WavesManager getWavesManager() {
+        return wavesManager;
     }
 }
